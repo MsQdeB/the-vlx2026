60a2457930485b24246c9dcd91f7feef3fe903cd
prosevenup3108 <longnhong3108@gmail.com>
2026-02-10 22:33:24 +0700

    update themee

--- src/app/feature/livebands/livebands.ts
+++ src/app/feature/livebands/livebands.ts
@@ -13,13 +13,13 @@ export class LivebandsComponent {
     {
       name: 'HAT JAZZ BAND',
       description:
-        '"Our very own local Jazz band whose positivity have made our past events so full of energy! Our local band, the HAT Jazz Band, simply the best Jazz band in Da Nang, Vietnam."',
+        '"Our very own local Jazz band whose positivity have made our past events so full of energy! Our local band, the <a href="https://www.facebook.com/hatjazzband" target="_blank" rel="noopener noreferrer">HAT Jazz Band</a>, simply the best Jazz band in Da Nang, Vietnam."',
       image: '/assets/live-bands/Band-HATJAZZBAND.jpg',
     },
     {
       name: 'ASIAN GOLDEN SWING QUARTET',
       description:
-        '"Coming all the way from Japan, Asian Golden Swing Quartet has been playing at big Swing Dance events and is loved by many dancers. Join us welcome their time in Vietnam!"',
+        '"Coming all the way from Japan, <a href="https://www.facebook.com/asiangoldenswingquartet" target="_blank" rel="noopener noreferrer">Asian Golden Swing Quartet</a> + <a href="https://www.facebook.com/profile.php?id=100003553186281" target="_blank" rel="noopener noreferrer">Ryohei</a> has been playing at big Swing Dance events and is loved by many dancers. Join us welcome their time in Vietnam!"',
       image: '/assets/live-bands/Band Asian-Golden-Swing-Quartet.jpg',
     },
   ];
