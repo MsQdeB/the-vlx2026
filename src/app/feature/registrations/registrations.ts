6898e2a4cdabc1555957fc6de3d25e2485ef2a3c
prosevenup3108 <longnhong3108@gmail.com>
2026-03-11 00:15:11 +0700

    update final vlx

--- src/app/feature/registrations/registrations.ts
+++ src/app/feature/registrations/registrations.ts
@@ -72,9 +72,9 @@ export class RegistrationsComponent implements OnInit, OnDestroy {
 
   onTshirtPreorderClick() {
     // Open t-shirt pre-order link (update with actual link when available)
-    window.open(
-      'https://docs.google.com/forms/d/e/1FAIpQLSemLT--n_0Qu-dTp9ffHaeSHsFXrNXP7pcUTjyLUYkTq7cIWw/viewform',
-      '_blank',
-    );
+    // window.open(
+    //   'https://docs.google.com/forms/d/e/1FAIpQLSemLT--n_0Qu-dTp9ffHaeSHsFXrNXP7pcUTjyLUYkTq7cIWw/viewform',
+    //   '_blank',
+    // );
   }
 }
